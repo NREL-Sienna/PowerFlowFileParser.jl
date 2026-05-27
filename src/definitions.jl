1b24812a17bdbb7881bee6ae086b20977f467889
Chubin <hchubin@nrel.gov>
2026-05-26 17:39:06 -0700

    copying all constants at the bottom of PSY/src/definitions.jl to this repos src/definitions.jl

--- src/definitions.jl
+++ src/definitions.jl
@@ -1,13 +1,24 @@
 const SKIP_PM_VALIDATION = false
+const PS_MAX_LOG = parse(Int, get(ENV, "PS_MAX_LOG", "50"))
+
+const DEFAULT_BASE_MVA = 100.0
+
+const POWER_SYSTEM_STRUCT_DESCRIPTOR_FILE =
+    joinpath(dirname(pathof(PowerSystems)), "descriptors", "power_system_structs.json")
+
+const DEFAULT_SYSTEM_FREQUENCY = 60.0
+
+const INFINITE_TIME = 1e4
+const START_COST = 1e8
+const INFINITE_COST = 1e8
+const INFINITE_BOUND = 1e6
+const BRANCH_BUS_VOLTAGE_DIFFERENCE_TOL = 0.01
 
 const PSSE_PARSER_TAP_RATIO_UBOUND = 1.5
 const PSSE_PARSER_TAP_RATIO_LBOUND = 0.5
 const PARSER_TAP_RATIO_CORRECTION_TOL = 1e-5
-const INFINITE_BOUND = 1e6
 
-const PS_MAX_LOG = parse(Int, get(ENV, "PS_MAX_LOG", "50"))
-
-const BRANCH_BUS_VOLTAGE_DIFFERENCE_TOL = 0.01
+const ZERO_IMPEDANCE_REACTANCE_THRESHOLD = 1e-4
 
 # Winding names for three-winding transformers
 const WINDING_NAMES = Dict(
